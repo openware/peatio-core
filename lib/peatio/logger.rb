671cf0bdd59152a2da0baf6b835fc7e211ef78e8
Camille <camille.meulien@gmail.com>
2018-07-24 09:47:57 +0200

    Add a logger with level configuration through LOG_LEVEL environment variable (#7)

---
+++ lib/peatio/logger.rb
@@ -0,0 +1,37 @@
+module Peatio
+  class Logger
+    class << self
+      def logger
+        @logger ||= ::Logger.new(STDERR, level: level)
+      end
+
+      def level
+        (ENV["LOG_LEVEL"] || "debug").downcase.to_sym
+      end
+
+      def debug(progname = nil, &block)
+        logger.debug(progname, &block)
+      end
+
+      def info(progname = nil, &block)
+        logger.info(progname, &block)
+      end
+
+      def warn(progname = nil, &block)
+        logger.warn(progname, &block)
+      end
+
+      def error(progname = nil, &block)
+        logger.error(progname, &block)
+      end
+
+      def fatal(progname = nil, &block)
+        logger.fatal(progname, &block)
+      end
+
+      def unknown(progname = nil, &block)
+        logger.unknown(progname, &block)
+      end
+    end
+  end
+end
