1c9b43052ebc45fa8722fca6b47a7b0d6f53d3f0
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2019-01-30 18:32:03 +0200

    Update

--- lib/peatio.rb
+++ lib/peatio.rb
@@ -18,4 +18,7 @@ module Peatio
   require_relative "peatio/injectors/peatio_events"
   require_relative "peatio/security/key_generator"
   require_relative "peatio/auth/jwt_authenticator"
+  require_relative "peatio/blockchain_service/base"
+  require_relative "peatio/blockchain_service/error"
+  require_relative "peatio/blockchain_service/ethereum"
 end
--- lib/peatio/blockchain_service/base.rb
+++ lib/peatio/blockchain_service/base.rb
@@ -1,5 +1,9 @@
 module Peatio::BlockchainService
   class Base
+    def initialize(options = {})
+      @opt = options
+    end
+
     def process_blockchain()
       method_not_implemented
     end
