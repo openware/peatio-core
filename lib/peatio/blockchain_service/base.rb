0a440555727b8bff6a0a3fdf9bc1f73c984f215c
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2019-01-31 18:24:31 +0200

    [WIP] Move Ethereum

--- lib/peatio/blockchain_service/base.rb
+++ lib/peatio/blockchain_service/base.rb
@@ -1,11 +1,48 @@
 module Peatio::BlockchainService
   class Base
-    def initialize(blockchain:)
-      @opt = options
+    attr_reader :logger, :cache, :blockchain, :currencies
+
+    def initialize(logger:, cache:, blockchain:, currencies:)
+      @logger = logger
+      @cache = cache
+      @blockchain = blockchain
+      @currencies = currencies
     end
 
-    def process_blockchain
+    # TODO: Doc
+    def fetch_block!
       method_not_implemented
+    end
+
+    # TODO: Doc
+    def filtered_deposits(addresses, &block)
+      method_not_implemented
+    end
+
+    # TODO: Doc
+    def filtered_withdrawals(txids, &block)
+      method_not_implemented
+    end
+
+    # TODO: Doc
+    def current_block
+      method_not_implemented
+    end
+
+    # TODO: Doc
+    def latest_block
+      method_not_implemented
+    end
+
+    # TODO: Tricky code!!!
+    # TODO: Doc
+    def client
+      @client ||= self.class.name.sub("Service", "Client").constantize.new(blockchain)
+    end
+
+    private
+    def cache_key(*suffixes)
+      [self.class.name.underscore.sub("/", ":"), suffixes].join(":")
     end
   end
 end
--- lib/peatio/blockchain_service/ethereum.rb
+++ lib/peatio/blockchain_service/ethereum.rb
@@ -1,5 +1,105 @@
 module Peatio::BlockchainService
   class Ethereum < Base
+
+    def fetch_block!
+      if blockchain.height >= latest_block
+        Rails.logger.info { "Skip synchronization. No new blocks detected height: #{blockchain.height}, latest_block: #{latest_block}" }
+        nil
+      end
+      # TODO: Do we need @current_block here ???
+      @current_block = blockchain.height
+      @block_json = client.get_block(@current_block)
+      if @block_json.blank? || @block_json['transactions'].blank?
+        # TODO: Do something special!!!
+      end
+    end
+
+    def latest_block
+      cache.fetch(cache_key(:latest_block), expires_in: 5.seconds) do
+        client.latest_block_number
+      end
+    end
+
+    def client
+      ::BlockchainClient::Ethereum.new(@blockchain)
+    end
+
+    def filtered_deposits(payment_addresses, &block)
+      block_json
+        .fetch('transactions')
+        .each_with_object([]) do |block_txn, deposits|
+
+        if block_txn.fetch('input').hex <= 0
+          txn = block_txn
+          next if client.invalid_eth_transaction?(txn)
+        else
+          txn = client.get_txn_receipt(block_txn.fetch('hash'))
+          next if txn.nil? || client.invalid_erc20_transaction?(txn)
+        end
+
+        payment_addresses
+          .select { |pa| pa.address = client.to_address(txn)}
+          .map do |payment_address|
+            deposit_txs = client.build_transaction(txn, block_json,
+                                                   payment_address.address,
+                                                   payment_address.currency)
+            deposit_txs.fetch(:entries).each do |entry|
+              # TODO: Move to peatio!
+              # if entry[:amount] <= payment_address.currency.min_deposit_amount
+              #   # Currently we just skip small deposits. Custom behavior will be implemented later.
+              #   Rails.logger.info do  "Skipped deposit with txid: #{deposit_txs[:id]} with amount: #{entry[:amount]}"\
+              #                          " from #{entry[:address]} in block number #{deposit_txs[:block_number]}"
+              #   end
+              #   next
+              # end
+              deposit = { txid:           deposit_txs[:id],
+                            address:        entry[:address],
+                            amount:         entry[:amount],
+                            member:         payment_address.account.member,
+                            currency:       payment_address.currency,
+                            txout:          entry[:txout],
+                            block_number:   deposit_txs[:block_number] }
+              block.call(deposit) if block_given? # Is it right ?
+              deposits << deposit
+            end
+          end
+      end
+    end
+
+    def filtered_withdrawals(withdrawals, &block)
+      block_json
+        .fetch('transactions')
+        .each_with_object([]) do |block_txn, withdrawals|
+
+        withdrawals
+          .select { |w| w.txid == client.normalize_txid(block_txn.fetch('hash')) }
+          .each do |withdraw|
+
+          # TODO: Check this.
+          # if block_txn.fetch('input').hex <= 0
+          #   txn = block_txn
+          #   next if client.invalid_eth_transaction?(txn)
+          # else
+          #   txn = client.get_txn_receipt(block_txn.fetch('hash'))
+          #   if txn.nil? || client.invalid_erc20_transaction?(txn)
+          #     withdraw.fail!
+          #     next
+          #   end
+          # end
+
+          withdraw_txs = client.build_transaction(txn, block_json, withdraw.rid, withdraw.currency)  # block_txn required for ETH transaction
+          withdraw_txs.fetch(:entries).each do |entry|
+            withdrawal =  { txid:           withdraw_txs[:id],
+                             rid:            entry[:address],
+                             amount:         entry[:amount],
+                             block_number:   withdraw_txs[:block_number] }
+            block.call(withdrawal) if block_given?
+            withdrawals << withdrawal
+          end
+        end
+      end
+    end
+
     # Rough number of blocks per hour for Ethereum is 250.
     def process_blockchain(blocks_limit: 250, force: false)
       latest_block = client.latest_block_number
